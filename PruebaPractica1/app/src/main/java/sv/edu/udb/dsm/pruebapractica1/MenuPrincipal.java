88f2f9a34214d697288c8ff7c68ae9b15a0d05ce
Luis Coto <luiscoto001@gmail.com>
2022-03-04 18:09:19 -0600

    Login y menu sin diseño
    No esta validado aun.

---
+++ PruebaPractica1/app/src/main/java/sv/edu/udb/dsm/pruebapractica1/MenuPrincipal.java
@@ -0,0 +1,41 @@
+package sv.edu.udb.dsm.pruebapractica1;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.strictmode.WebViewMethodCalledOnWrongThreadViolation;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MenuPrincipal extends AppCompatActivity {
+
+    TextView tvSaludo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+
+        tvSaludo = (TextView) findViewById(R.id.tvSaludo);
+        cargarPreferencias();
+    }
+    public void cargarPreferencias(){
+        SharedPreferences preferences=getSharedPreferences
+                ("credenciales", Context.MODE_PRIVATE);
+        String usuario = preferences.getString("usuario","No existe un usuario");
+        String contra = preferences.getString("contraseña","No existe una contraseña");
+        tvSaludo.setText( "Bienvenido "+ usuario);
+    }
+    public void onClickPrimero(View v){
+        finish();
+    }
+    public void onClickSegundo(View v){
+        finish();
+    }
+
+    public void finalizar(View v){
+        finish();
+    }
+}
