cdc6c40591b10a12ca8a0c7920471455c1e8445f
Omar-PJ <bryanomarperez2000@gmail.com>
2022-03-06 01:13:02 -0600

    Agregando ultimas validaciones y diseño

--- PruebaPractica1/app/src/main/java/sv/edu/udb/dsm/pruebapractica1/Comisiones.java
+++ PruebaPractica1/app/src/main/java/sv/edu/udb/dsm/pruebapractica1/Comisiones.java
@@ -1,12 +1,15 @@
 package sv.edu.udb.dsm.pruebapractica1;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Comisiones extends AppCompatActivity {
         TextView Nombre, Codigo, Mes, Totalven, Porcomision, TotalCom;
+        ImageView Foto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,9 @@ public class Comisiones extends AppCompatActivity {
         Totalven=findViewById(R.id.txtTotalv);
         Porcomision=findViewById(R.id.txtcomision);
         TotalCom=findViewById(R.id.txtTotalc);
+        Foto=findViewById(R.id.idfoto);
+
+
 
         Bundle datos = getIntent().getExtras();
         String nombre = datos.getString("Name");
@@ -28,45 +34,48 @@ public class Comisiones extends AppCompatActivity {
         Nombre.setText(nombre);
         Codigo.setText(Cod);
         Mes.setText(Mess);
-        Totalven.setText(String.valueOf(TotalV));
+        Totalven.setText(String.valueOf("$ "+TotalV));
+        //Calculado % de comision y total de comision
         if(TotalV<500){
         comicion=0.0;
         Porcomision.setText(comicion*100+" %");
             Totalcomi=TotalV*comicion;
-            TotalCom.setText(String.valueOf(Totalcomi));
+            TotalCom.setText(String.valueOf("$ "+Totalcomi));
         }else
             if (TotalV>=500 && TotalV<1000){
             comicion=0.05;
             Porcomision.setText(comicion*100+" %");
             Totalcomi=TotalV*comicion;
-            TotalCom.setText(String.valueOf(Totalcomi));}
+            TotalCom.setText(String.valueOf("$ "+Totalcomi));}
             else
                 if(TotalV>=1000 && TotalV<2000){
                     comicion=0.1;
                     Porcomision.setText(comicion*100+" %");
                     Totalcomi=TotalV*comicion;
-                    TotalCom.setText(String.valueOf(Totalcomi));
+                    TotalCom.setText(String.valueOf("$ "+Totalcomi));
                 }
                 else
                     if(TotalV>=2000 && TotalV<3000){
                         comicion=0.15;
                         Porcomision.setText(comicion*100+" %");
                         Totalcomi=TotalV*comicion;
-                        TotalCom.setText(String.valueOf(Totalcomi));
+                        TotalCom.setText(String.valueOf("$ "+Totalcomi));
                     }
                     else
                         if(TotalV>=3000 && TotalV<4000){
                             comicion=0.2;
                             Porcomision.setText(comicion*100+" %");
                             Totalcomi=TotalV*comicion;
-                            TotalCom.setText(String.valueOf(Totalcomi));
+                            TotalCom.setText(String.valueOf("$ "+Totalcomi));
                         }
                         else{
                             comicion=0.3;
                             Porcomision.setText(comicion*100+" %");
                             Totalcomi=TotalV*comicion;
-                            TotalCom.setText(String.valueOf(Totalcomi));
+                            TotalCom.setText(String.valueOf("$ "+Totalcomi));
                         }
+        Uri pathImag=getIntent().getData();
+        Foto.setImageURI(pathImag);
         }
 
     public void finalizarActividad(View v){finish();}
--- PruebaPractica1/app/src/main/java/sv/edu/udb/dsm/pruebapractica1/Ejercicio1.java
+++ PruebaPractica1/app/src/main/java/sv/edu/udb/dsm/pruebapractica1/Ejercicio1.java
@@ -1,38 +1,84 @@
 package sv.edu.udb.dsm.pruebapractica1;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.constraint.solver.widgets.analyzer.Direct;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Ejercicio1 extends AppCompatActivity {
     EditText Nombre, Id, Total;
+    ImageView imagen;
     Spinner Meses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio1);
-        Nombre = findViewById(R.id.txtName);
-        Id = findViewById(R.id.txtId);
-        Total = findViewById(R.id.txtTotal);
-        Meses = findViewById(R.id.ListMes);
+
+        imagen=findViewById(R.id.IdImag);
     }
         public void Calcular(View view) {
-            String Name= Nombre.getText().toString();
-            String Codigo= Id.getText().toString();
-            Double Tot= Double.valueOf(Total.getText().toString());
-            String Mes= Meses.getSelectedItem().toString();
-            Intent intent = new Intent(this,Comisiones.class);
-            intent.putExtra("Name",Name);
-            intent.putExtra("Codigo",Codigo);
-            intent.putExtra("Tot",Tot);
-            intent.putExtra("Mes",Mes);
-            startActivity(intent);
+            Nombre = findViewById(R.id.txtName);
+            Id = findViewById(R.id.txtId);
+            Total = findViewById(R.id.txtTotal);
+            Meses = findViewById(R.id.ListMes);
+
+            if(TextUtils.isEmpty(Nombre.getText()) && TextUtils.isEmpty(Id.getText()) && TextUtils.isEmpty(Total.getText())){
+                Nombre.setError("Campo Vacio");
+                Id.setError("Campo Vacio");
+                Total.setError("Campo Vacio");
+            }else if(TextUtils.isEmpty(Id.getText().toString()) && TextUtils.isEmpty(Total.getText())){
+                Id.setError("Campo Vacio");
+                Total.setError("Campo Vacio");
+            }else if(TextUtils.isEmpty(Total.getText().toString())&& TextUtils.isEmpty(Nombre.getText())){
+                Total.setError("Campo Vacio");
+                Nombre.setError("Campo Vacio");
+            }else if(TextUtils.isEmpty(Nombre.getText().toString())&& TextUtils.isEmpty(Id.getText())){
+                Id.setError("Campo Vacio");
+                Nombre.setError("Campo Vacio");
+            }
+            else if (TextUtils.isEmpty(Nombre.getText())){
+                Nombre.setError("Campo Vacio");
+            }else if (TextUtils.isEmpty(Id.getText())){
+                Id.setError("Campo Vacio");
+            }else if (TextUtils.isEmpty(Total.getText())){
+                Total.setError("Campo Vacio");
+            }else
+                {CargarImagen();}
         }
     public void finalizarActividad(View v){finish();}
 
+    public void CargarImagen() {
+        Intent intent= new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent,"Selecione la aplicacion:"),10);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK){
+            Uri path=data.getData();
+            imagen.setImageURI(path);
+            Intent intent = new Intent(this, Comisiones.class);
+            intent.putExtra("Name", Nombre.getText().toString());
+            intent.putExtra("Codigo",Id.getText().toString() );
+            intent.putExtra("Tot",Double.valueOf(Total.getText().toString()));
+            intent.putExtra("Mes", Meses.getSelectedItem().toString());
+            intent.setData(path);
+            startActivity(intent);
+        }
+    }
 }
